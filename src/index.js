9ccce7f2a95098889b4d6340fd6202819da8e4f2
Rene <renebitter@gmail.com>
2020-08-04 10:56:58 -0300

    npm package fix

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-//Test
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
